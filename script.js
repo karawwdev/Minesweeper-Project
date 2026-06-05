d2a2be13fc9d826c040b4e3231965edbf5838d5a
Kara <karaduffy.dev@gmail.com>
2026-06-04 21:23:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Initalize the rows and columns (make these user-input later?)
+let numRows = 10;
+let numColumns = 10;
+let numMines = 10;
+
+
+// Populate the grid
+function populateGrid() {
+  for(let i = 0; i < numRows*numColumns; i++) {
+    const cell = document.createElement('div');
+  }
+}
+populateGrid()
