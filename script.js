7abfc11f3b66535ddca57a9cf5e35b11f221f4da
Kara Duffy <karaduffy.dev@gmail.com>
2026-07-15 20:52:28 -0700

    1st commit

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 let numRows = 10;
 let numColumns = 10;
 let numMines = 10;
+const board = document.querySelector('#board');
 
-
-// Populate the grid
-function populateGrid() {
+// Populate the board
+function populateBoard() {
   for(let i = 0; i < numRows*numColumns; i++) {
-    const cell = document.createElement('div');
+    const newDiv = document.createElement('div'); // Create a new child div to represent each cell in the board
+    newDiv.id = `cell${i+1}`; // Give the cells unique IDs
+    board.appendChild(newDiv); // Append the cells to the board
   }
 }
-populateGrid()
+
+populateBoard();
