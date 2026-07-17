16dde5ddac59a229e18472fd3249e7c4b43809b5
Kara Duffy <karaduffy.dev@gmail.com>
2026-07-16 18:05:46 -0700

    Populate board on difficulty button clicks

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let numRows = 10;
 let numColumns = 10;
 let numMines = 10;
 const board = document.querySelector('#board');
+const diffButtons = document.querySelector('.game-buttons')
 
 // Populate the board
 function populateBoard() {
+  board.querySelectorAll(":scope > div").forEach(div => div.remove());
   for(let i = 0; i < numRows*numColumns; i++) {
     const newDiv = document.createElement('div'); // Create a new child div to represent each cell in the board
     newDiv.id = `cell${i+1}`; // Give the cells unique IDs
     board.appendChild(newDiv); // Append the cells to the board
   }
 }
-
-populateBoard();
