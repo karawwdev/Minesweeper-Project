621023f1965660d859d39f02c0ea21357bc0f4df
Kara Duffy <karaduffy.dev@gmail.com>
2026-07-18 20:26:04 -0700

    numMines scales with difficulty buttons

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ let numRows = 10;
 let numColumns = 10;
 let numMines = 10;
 const board = document.querySelector('#board');
-const diffButtons = document.querySelector('.game-buttons')
 
 // Populate the board
-function populateBoard() {
+function populateBoard(gameDiff) {
   board.querySelectorAll(":scope > div").forEach(div => div.remove());
+  if(gameDiff === 'easy') {
+    numMines=10; 
+  } else if(gameDiff === 'med') {
+    numMines=15;
+  } else if(gameDiff === 'hard') {
+    numMines=20;
+  } else {
+    console.error();
+  };
+  console.log(numMines);
   for(let i = 0; i < numRows*numColumns; i++) {
     const newDiv = document.createElement('div'); // Create a new child div to represent each cell in the board
     newDiv.id = `cell${i+1}`; // Give the cells unique IDs
